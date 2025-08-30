f65bd9695c255384db49f195bca9b15ed7559958
nermine.baa <benabdarrahmennermine@gmail.com>
2025-08-30 17:41:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit as st
 from snowflake.snowpark import Session
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
+
 
 # --- App title ---
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
