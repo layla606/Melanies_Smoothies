85a1488635625f1aa0998b11ea424575b496823a
nermine.baa <benabdarrahmennermine@gmail.com>
2025-09-04 11:44:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ session = Session.builder.configs(sf_config).create()
 
 # --- Load fruit options from Snowflake ---
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe,use_container_width= True)
-#st.stop()
+st.dataframe(data=my_dataframe,use_container_width= True)
+st.stop()
 #convert the snowpark dataframe to a pandas dataframe so we can use the loc function
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
